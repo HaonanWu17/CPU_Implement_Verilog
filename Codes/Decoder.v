dd864322ee6659800449f035c3d67c589b9fb5d8
Siyuan Jing <crazycyy233@gmail.com>
2024-11-06 13:01:00 -0500

    Update Decoder.v

--- Codes/Decoder.v
+++ Codes/Decoder.v
@@ -1,1 +1,20 @@
-
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Siyuan Jing & Haonan Wu
+// 
+// Create Date: 11/04/2024 05:51:45 PM
+// Design Name: 
+// Module Name: Main_control_testbench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
